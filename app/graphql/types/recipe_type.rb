472569690d4816b60814e300ebfeca2301c7abf0
Ian Brennan <ian.brennan@viget.com>
2019-09-22 15:49:39 -0500

    Ingredients model added, tested

--- app/graphql/types/recipe_type.rb
+++ app/graphql/types/recipe_type.rb
@@ -3,5 +3,6 @@ module Types
     field :id, ID, null: false
     field :name, String, null: true
     field :user, Types::UserType, null: false
+    field :ingredients, [Types::IngredientType], null: true
   end
 end
---
+++ db/migrate/20190921235052_create_ingredients.rb
@@ -0,0 +1,10 @@
+class CreateIngredients < ActiveRecord::Migration[6.0]
+  def change
+    create_table :ingredients do |t|
+      t.string :name
+      t.float :amount
+      t.string :unit
+      t.references :recipe, null: false, foreign_key: true
+    end
+  end
+end
